bf0c6238e17eefec9623c6ae59ba827f02b40610
aiyuanl <34074762+aiyuanl@users.noreply.github.com>
2019-02-20 00:36:09 -0800

    123

---
+++ app/src/main/java/com/example/medai/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.medai;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.medai.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString(); // now we have the id number
+        //replace this, and based on id number, open the file, read the file and find the specific patient data and display the history chart
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
+}
